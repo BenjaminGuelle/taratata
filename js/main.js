c4152b468d15ebe95ee1ceae9bf804905f574a86
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-07-15 19:30:59 +0200

    menu-mobile : make animation & content

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 console.log('Hello ben');
 
+// BTN 
+let btnMobileOpened = document.querySelector('#btn-mobile-opened');
+let btnMobileClose = document.querySelector('.btn-mobile-close');
+
+// ELEMENTS
+let menuMobile = document.querySelector('#mobile-wrapper');
+let closeIcon = document.querySelector('.close-icon');
+
+// console.log(btnMobileOpened);
+
+btnMobileOpened.addEventListener('click', handleClickBtnMobile);
+btnMobileClose.addEventListener('click', handleClickBtnMobile);
+
+function handleClickBtnMobile() {
+    if (menuMobile.classList.contains('menu-opened')) {
+        menuMobile.classList.remove('menu-opened');
+        closeIcon.classList.remove('menu-opened');
+    } else {
+        menuMobile.classList.add('menu-opened');
+        closeIcon.classList.add('menu-opened');
+    }
+
+}
