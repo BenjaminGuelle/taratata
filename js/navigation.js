8101e2216f1944c1b3c215df851d9f5345f653a9
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-07-22 17:24:37 +0200

    animation : submenu & menu-mobile done

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,18 @@
-// ADD EVENTS
+/***********************************
+ ************ ADD EVENTS ***********
+ ***********************************
+ */
+
+// ----- navbar event
 const initActionNavbar = () => {
     let btnMobileOpened = document.querySelector('#btn-mobile-opened');
     btnMobileOpened.addEventListener('click', handleClickBtnMobile);
+    // menu mobile
     let btnMobileClose = document.querySelector('.btn-mobile-close');
     btnMobileClose.addEventListener('click', handleClickBtnMobile);
 }
 
+// ----- panel event
 const initActionPanel = () => {
     let panelNext = document.querySelectorAll('.panel-next');
     panelNext.forEach((item) => (
@@ -15,18 +22,25 @@ const initActionPanel = () => {
     panelBack.forEach((item) => {
         item.addEventListener('click', handleSwitchPanel);
     });
+    // event to anim back first panel menu mobile
+    let animBackFirstPanel = document.querySelector('#panel-back-first');
+    animBackFirstPanel.addEventListener('click', handleSwitchBackAnim);
 }
 
+// ----- submenu event
 const initHoverSubmenu = () => {
     let linkBoutique = document.querySelector('#hover-boutique');
     linkBoutique.addEventListener('mouseover', handleHoverSubmenu);
 }
-// END 
 
-// METHOD LIST
+/***********************************
+ *********** METHOD LIST ***********
+ ***********************************
+ */
 
 // Display menu mobile
 const handleClickBtnMobile = () => {
+    TLFadeMobile.restart();
     let menuMobile = document.querySelector('#mobile-wrapper');
     let closeIcon = document.querySelector('.close-icon');
     if (menuMobile.classList.contains('menu-opened')) {
@@ -36,7 +50,6 @@ const handleClickBtnMobile = () => {
     } else {
         menuMobile.classList.add('menu-opened');
         closeIcon.classList.add('menu-opened');
-        
     }
 }
 
@@ -76,41 +89,48 @@ const handleSwitchPanel = (e) => {
     }
 }
 
-let submenuItems = document.querySelectorAll('.submenu-list .submenu-item');
+const handleSwitchBackAnim = () => {
+    console.log('START ANIM BACK PANEL');
+    TLFadeMobile.restart();
+}
+
+const handleHoverSubmenu = () => {
+    TLFadeSub.restart();
+}
+
+/***********************************
+ ************ Timelines ************
+ ***********************************
+ */
+
+ // ----- Submenu animation
 let submenuItems1 = document.querySelectorAll('.submenu-list-1 .submenu-item');
 let submenuItems2 = document.querySelectorAll('.submenu-list-2 .submenu-item');
+let submenuSliderTitle = document.querySelector('.submenu-collection .submenu-item');
+let submenuSlider = document.querySelector('.submenu-collection .slider-collection');
 
-const timelineConfig = () => {
-    for (let i = 0; i < submenuItems.length; i++) {
-        if (i < submenuItems1.length ) {
-            TL.to(submenuItems[i], {duration: 0.2})
-        } else if (i == submenuItems1.length) {
-            TL.to(submenuItems[i], {duration: 0.2}, 0);
-        } else {
-            TL.to(submenuItems[i], {duration: 0.2}, '>')
-        }
-    }
-}
-
-const TL = gsap.timeline({
-    defaults: {
-        // duration: 0.2,
-        ease: "power2",
-        autoAlpha: 1,
-        y: 0,
-    },
+const TLFadeSub = gsap.timeline({
     paused: true,
-    onStart: () => console.log('ANIME START'),
-    onComplete: () => console.log('ANIME FINI')
-});
+})
 
-TL
-.add(timelineConfig())
+TLFadeSub
+.to(submenuItems1, {autoAlpha: 1, y: 0, delay: 0.25, duration: 0.20, stagger: 0.1})
+.to(submenuItems2, {autoAlpha: 1, y: 0, delay: 0.25, duration: 0.20, stagger: 0.1}, '0')
+.to(submenuSliderTitle, {autoAlpha: 1, y: 0, delay: 0.25, duration: 0.20}, '0')
+.to(submenuSlider, {autoAlpha: 1, duration: 0.30}, '-=0.50')
 
+// ----- Menu mobile animation
+let btnMobileClose = document.querySelector('.btn-mobile-close');
+let panelList = document.querySelectorAll('.panel-list-1 .panel-item');
+let panelLine = document.querySelector('.panel-br');
 
-// Submenu
-const handleHoverSubmenu = () => {
-    TL.restart()
-    console.log('submenu 1', submenuItems1);
-    console.log('submenu 2', submenuItems2);
-}
+const TLFadeMobile = gsap.timeline({
+    paused: true,
+    onStart: () => console.log('START MENU MOBILE'),
+    onComplete: () => console.log('COMPLETE MENU MOBILE'),
+})
+
+TLFadeMobile
+.to(btnMobileClose, {autoAlpha: 1, delay: 0.2, duration: 0.20})
+.to(panelList, {autoAlpha: 1, delay: 0.2, duration: 0.20, stagger: 0.1}, 0)
+.to(panelLine, {autoAlpha: 1, width: '100%', delay: 0.5, duration: 0.50}, '-=0.75')
