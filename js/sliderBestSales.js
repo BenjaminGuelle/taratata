d8feb8c41ce9db2a2d6c576dfd0af7bf4141779c
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-09-27 17:26:00 +0200

    home : best saler : include splide librairie && slide best sales done

---
+++ js/sliderBestSales.js
@@ -0,0 +1,13 @@
+new Splide( '.splide', {
+    perPage: 3,
+    breakpoints: {
+        '640': {
+            perPage: 2,
+            gap    : '1rem',
+        },
+        '480': {
+            perPage: 1,
+            gap    : '1rem',
+        },
+    }
+} ).mount();
